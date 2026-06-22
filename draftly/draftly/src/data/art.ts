a575bbf5ecb98d1021cc3f31684d31b6452eff14
Eugen <y75r2zdmv2@privaterelay.appleid.com>
2026-06-22 23:53:25 +0200

    Delete draftly/draftly directory

--- draftly/draftly/src/data/art.ts
+++
@@ -1,7 +0,0 @@
-// Champion art URLs, sourced from the active champion-data provider
-// (Data Dragon). Components import these instead of touching providers.
-import { championDataProvider } from './providers/registry'
-
-export const splashUrl = (id: string) => championDataProvider.splashUrl(id)
-export const loadingUrl = (id: string) => championDataProvider.loadingUrl(id)
-export const squareUrl = (id: string) => championDataProvider.squareUrl(id)
